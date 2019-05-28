104ece8816fb8402a45590c51bec5b5995a81b44
maxsunnyday <max.sun@yale.edu>
2019-05-28 15:29:06 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    delimiters = [".", "!", "?"]
+    sentences = self.split(Regexp.union(delimiters))
+    sentences.delete_if { |sentence|
+      sentence.empty?
+    }
+    sentences.length
   end
 end
