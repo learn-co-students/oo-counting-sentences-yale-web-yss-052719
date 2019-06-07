5ab8feaf1596f9824f0478e0f3686e47b244595a
Alex Alanya <alexandro.alanya@gmail.com>
2019-06-07 13:55:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,16 +1,24 @@
 require 'pry'
 
 class String
+  
+  attr_reader :sentence?, 
+  
+  def initialize(sentence)
 
   def sentence?
-    
+    if @self = self.sentence?.end = 0
+      puts true 
+    end
   end
 
   def question?
-
+    
+    
   end
 
   def exclamation?
+    
 
   end
 
@@ -18,3 +26,4 @@ class String
 
   end
 end
+
